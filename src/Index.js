c7d8a24b8ba1040692318cf3cf680143691bd056
Isha Panwar <125899495+panwarisha30@users.noreply.github.com>
2023-11-02 10:12:38 +0000

    add index.css

--- src/Index.js
+++ src/Index.js
@@ -1,5 +1,19 @@
+var result=document.getElementById("result");
 function display(num)
 {
-    alert(num);
+    result.value +=num;/* '+' here means that the data previously stored should also be there and new data should also be there.*/
 }
-alert();
+function calculate_values()
+{
+   var final_num=result.value;
+   var final_result=eval(final_num);/* Here we have used eval function of js to evaluate reult*/ 
+   result.value=final_result;
+}
+function allclear()
+{
+    result.value=""; /*It clears the entire value entered by user*/
+}
+function del()
+{
+    result.value=result.value.slice(0,-1); /*The value which we will pass is deleted one by one with the help of slice function provided by js*/ 
+}
